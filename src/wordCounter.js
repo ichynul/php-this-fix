0298dea89068c603efc05b4c3ad6541c39ec3aaa
ichynul <ichynul@outlook.com>
2018-05-01 13:27:50 +0800

    @

--- src/wordCounter.js
+++ src/wordCounter.js
@@ -8,51 +8,51 @@ class WordCounter {
         var vscode = this.vscode;
         var StatusBarAlignment = vscode.StatusBarAlignment;
         var window = this.vscode.window;
-
-        //statusBar，是需要手动释放的
-        this.statusBar = window.createStatusBarItem(StatusBarAlignment.Left);
-
-        //跟注册事件相配合的数组，事件的注册，也是需要释放的
+        this.diagnosticCollection = vscode.languages.createDiagnosticCollection("build");
         var disposable = [];
-        //事件在注册的时候，会自动填充一个回调的dispose到数组
-        window.onDidChangeTextEditorSelection(this.count, this, disposable);
-
-        //保存需要释放的资源
+        window.onDidChangeTextEditorSelection(this.check, this, disposable);
+        window.onDidChangeActiveTextEditor(this.check, this, disposable);
         this.disposable = vscode.Disposable.from(disposable);
-
-        this.updateText();
-        this.statusBar.show();
+        this.check();
     }
 
-    updateText() { //现在快凌晨两点，偷个懒早点睡，临时改成字符数量了。
-        var window = this.vscode.window;
-        this.editor = window.activeTextEditor;
-        let selection = editor.selection;
-        let content = editor.document.getText(selection);
-        var len = content.replace(/[\r\n\s]+/g, '').length;
-        this.statusBar.text = $this.count();
-    }
-
-    showMsg() {
-        vscode.window.showInformationMessage($this.count());
-    }
-
-    dispose() { //实现dispose方法
-        this.disposable.dispose();
-        this.statusBar.dispose();
-    }
-
-    count() {
-        let editor = vscode.window.activeTextEditor;
+    check() {
+        let editor = this.vscode.window.activeTextEditor;
         if (!editor) {
-            return 'No editor';
+            return;
         }
         let selection = editor.selection;
-        let content = editor.document.getText(selection);
+        let content = editor.document.getText(selection) || editor.document.getText();
         if (!content.length) {
-            return 'No text';
+            return;
         }
-        var matchs = text.match(/\$this->\$/g) || [];
-        return `$this->$:' ${matchs.length}`;
+        let diagnostics = [];
+        let lines = content.split(/\r?\n/g);
+        lines.forEach((line, i) => {
+            let index = line.indexOf('$this->$');
+            if (index >= 0) {
+                diagnostics.push({
+                    severity: this.vscode.workspace.getConfiguration('warnningLevel'),
+                    range: {
+                        start: {
+                            line: i,
+                            character: index
+                        },
+                        end: {
+                            line: i,
+                            character: index + 8
+                        }
+                    },
+                    message: `\'${line.substr(index, 8)}\' is wrong maybe!`,
+                    source: 'ex'
+                });
+            }
+        });
+        this.diagnosticCollection.set(editor.document.uri, diagnostics);
+    }
+    dispose() {
+        this.disposable.dispose();
+        this.diagnosticCollection.dispose();
     }
 }
+module.exports = WordCounter;
